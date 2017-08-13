a33cc85b2724878ac28e0a155ab6fd49e07146ab
M Manzur Morshed <mmorshed@asu.edu>
2017-08-12 21:54:08 -0400

    Create excel_column_number.py

---
+++ excel_column_number.py
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def titleToNumber(self, A):
+        import string
+        letters = {}
+        i =	1
+        for	key in string.ascii_uppercase:
+            letters[key] = i
+            i = i+1
+        column_name	= list(A)    
+        column_number = 0 
+        for	i in range(0,len(column_name)):
+            j = abs(len(column_name)-i-1)
+            column_number += letters[column_name[i]] * math.pow(26,j)
+        return int(column_number)    
