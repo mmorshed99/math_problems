25e21588b305864566f7dcb2be419ecf14ffc0a9
M Manzur Morshed <mmorshed@asu.edu>
2018-10-16 21:12:26 -0400

    Update grid_unique_path.py

--- grid_unique_path.py
+++ grid_unique_path.py
@@ -20,3 +20,19 @@ class Solution:
             for j in range(B):
                 save[i].append(-1)
         return find(0,0)
+####alternative one avoiding recursion
+def uniquePaths(self, A, B):
+        save = []
+        for i in range(A):
+            save.append([])
+            for j in range(B):
+                save[i].append(0)
+        save[0][0] = 1
+        for i in range(1,A):
+            save[i][0] = 1
+        for i in range(1,B):
+            save[0][i] = 1
+        for i in range(1,A):
+            for j in range(1,B):
+                save[i][j] = save[i-1][j] + save[i][j-1]
+        return save[A-1][B-1]
